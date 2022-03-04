6e7b7ee0f05808c4fa58c2278c4966a4b2713e7a
Jair-Luna <lunajair@gmail.com>
2022-03-04 09:19:55 -0500

    F: Construyendo formulario de registro

--- lib/pages/register_page.dart
+++ lib/pages/register_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class RegisterPage extends StatefulWidget {
   RegisterPage({Key? key}) : super(key: key);
@@ -10,19 +11,68 @@ class RegisterPage extends StatefulWidget {
 }
 
 class _RegisterPageState extends State<RegisterPage> {
+  final _nameTextController = TextEditingController();
+  final _emailTextController = TextEditingController();
+  final _passwordTextController = TextEditingController();
+  final _phoneTextController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Center(
-          child: Text("¡Reistrate!"),
+          child: Text("¡Regístrate!"),
         ),
       ),
       body: SingleChildScrollView(
         padding: const EdgeInsets.all(16),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[],
+          children: <Widget>[
+            const SizedBox(height: 20),
+            TextField(
+              controller: _nameTextController,
+              cursorColor: Colors.white,
+              textInputAction: TextInputAction.next,
+              decoration: const InputDecoration(label: Text('Nombre')),
+            ),
+            const SizedBox(height: 20),
+            TextField(
+              controller: _emailTextController,
+              cursorColor: Colors.white,
+              textInputAction: TextInputAction.next,
+              decoration: const InputDecoration(label: Text('Email')),
+            ),
+            const SizedBox(height: 20),
+            TextField(
+              controller: _passwordTextController,
+              cursorColor: Colors.white,
+              textInputAction: TextInputAction.next,
+              decoration: const InputDecoration(label: Text('Contraseña')),
+            ),
+            const SizedBox(height: 20),
+            TextField(
+              controller: _phoneTextController,
+              cursorColor: Colors.white,
+              textInputAction: TextInputAction.next,
+              decoration: const InputDecoration(label: Text('Teléfono')),
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton.icon(
+              onPressed: () {},
+              style: ElevatedButton.styleFrom(
+                minimumSize: const Size.fromHeight(50),
+              ),
+              icon: const FaIcon(
+                FontAwesomeIcons.check,
+                size: 32,
+              ),
+              label: const Text(
+                'Registrarse',
+                style: TextStyle(fontSize: 24),
+              ),
+            )
+          ],
         ),
       ),
     );
